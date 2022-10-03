28cededb1bc3e3a0f1db5125ba7a4533ca12506d
salvadorM271 <guardianes27@outlook.com>
2022-10-03 16:12:16 -0600

    test

--- main.tf
+++ main.tf
@@ -291,6 +291,7 @@ resource "aws_api_gateway_method_response" "opt_http_status_value" {
 }
 
 resource "aws_api_gateway_integration_response" "opt_integration_response" {
+  depends_on = [aws_api_gateway_method_response.opt_http_status_value]
   rest_api_id = "${aws_api_gateway_rest_api.apiLambda.id}"
   resource_id = "${aws_api_gateway_resource.writeResource.id}"
   http_method = "${aws_api_gateway_method.opt_method.http_method}"
