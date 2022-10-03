de59aa0277b5fb5194f1edf644954449420b8b05
salvadorM271 <guardianes27@outlook.com>
2022-10-02 22:10:02 -0600

    fixing version error

--- main.tf
+++ main.tf
@@ -3,11 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "4.33.0"
     }
   }
   required_version = ">= 0.12"
